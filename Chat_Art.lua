8da96d8513def7d6daa77aff326177c199450317
BinaryDex <82985536+BinaryDex@users.noreply.github.com>
2022-08-15 15:26:23 -0400

    Update and rename art_letters.lua to Chat_Art.lua

--- Chat_Art.lua
+++ Chat_Art.lua
@@ -1,3 +1,7 @@
+local prefix = "."
+
+
+
 local letters = {
     ["a"] = {
         "⬛⬛⬛⬛⬛",
@@ -233,42 +237,6 @@ local letters = {
         "⬛🔴🔴🔴⬛",
         "⬛⬛⬛⬛⬛",
     },
-    ["+"] = {
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛🔴⬛⬛",
-        "⬛🔴🔴🔴⬛",
-        "⬛⬛🔴⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-    }
-    ["="] = {
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛🔴🔴🔴⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛🔴🔴🔴⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-    }
-    ["-"] = {
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛🔴🔴🔴⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-    }
-    ["_"] = {
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛⬛⬛⬛⬛",
-        "⬛🔴🔴🔴⬛",
-        "⬛⬛⬛⬛⬛",
-    }
     [" "] = {
         "⬛⬛⬛⬛⬛",
         "⬛⬛⬛⬛⬛",
@@ -279,3 +247,42 @@ local letters = {
         "⬛⬛⬛⬛⬛",
     }
 }
+
+
+local gmt = getrawmetatable(game)
+setreadonly(gmt, false)
+local oldNameCall = gmt.__namecall
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = 'Prefix your message with '.. prefix,
+    Text = 'Made by LabGuy94',
+    Icon = '',
+    Duration = 5,
+})
+
+DexHub:Notification("DexHub ☕", "Send Chat Art with "..prefix.."MESSAGE.\nIt does not support special characters like { ] - + etc.")
+local sound = Instance.new("Sound", game.Workspace)
+sound.SoundId = "rbxassetid://9128519965"
+sound:Play()
+
+gmt.__namecall = newcclosure(function(self, ...)
+    local args = {...}
+    if tostring(self) == "SayMessageRequest" and tostring(getnamecallmethod()) == "FireServer" then
+        local msg = args[1]
+        if string.sub(msg,1,string.len(prefix)) == prefix then
+            local message = string.sub(msg,string.len(prefix)+1)
+            local final = {"","","","","","",""}
+            for i = 1, #message do
+                local l = message:sub(i,i)
+                for x, line in pairs(letters[string.lower(l)]) do
+                    final[x] = final[x] .. line
+                end
+            end
+            for _, v in pairs(final) do
+                game:GetService("ReplicatedStorage")["DefaultChatSystemChatEvents"].SayMessageRequest:FireServer(v, "All")
+            end
+            return nil
+        end
+    end
+    return oldNameCall(self, ...)
+end)
